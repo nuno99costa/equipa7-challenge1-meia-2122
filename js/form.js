84de4b052897a954719d88fe8d72c3466dc0797d
Nuno Costa <nuno.costa@faro.com>
2021-11-22 19:47:23 +0000

    Add Prolog Source

--- js/form.js
+++ js/form.js
@@ -1,26 +1,39 @@
 $(document).ready(function () {
-    $("form").submit(function (event) {
+    $("#btnSubmit").click(function(){
         var formData = {
-            "conn_state": $("#conn_state"),
-            "duration": $("#duration"),
-            "history": $("#history"),
-            "id_orig_p": $("#id_orig_p"),
-            "id_resp_p": $("#id_resp_p"),
-            "missed_bytes": $("#missed_bytes"),
-            "orig_bytes": $("#orig_bytes"),
-            "orig_ip_bytes": $("#orig_ip_bytes"),
-            "orig_pkts": $("#orig_pkts"),
-            "resp_bytes": $("#resp_bytes"),
-            "resp_ip_bytes": $("#resp_ip_bytes"),
-            "resp_pkts": $("#resp_pkts"),
-            "service": $("#service"),
+            "conn_state": $("#conn_state").val(),
+            "duration": $("#duration").val(),
+            "history": $("#history").val(),
+            "id_orig_p": $("#id_orig_p").val(),
+            "id_resp_p": $("#id_resp_p").val(),
+            "missed_bytes": $("#missed_bytes").val(),
+            "orig_bytes": $("#orig_bytes").val(),
+            "orig_ip_bytes": $("#orig_ip_bytes").val(),
+            "orig_pkts": $("#orig_pkts").val(),
+            "resp_bytes": $("#resp_bytes").val(),
+            "resp_ip_bytes": $("#resp_ip_bytes").val(),
+            "resp_pkts": $("#resp_pkts").val(),
+            "service": $("#service").val(),
         };
-
+        var server = "";
+        var a = $("#prolog_check").is(":checked");
+        if(a){
+            server = "http://localhost:8081/iotintrusiondetection";
+        }else{
+            server = "http://localhost:8080/iotintrusiondetection";
+        }
         $.ajax({
             type: "GET",
-            url: "http://localhost:8080/iotintrusiondetection",
+            url: server,
             data: jQuery.param(formData)
         }).done(function (data) {
+            var text = "";
+            for(var key in data){
+                text += key+": "+data[key]+ "%\r\n";
+            }
+            var obj = $('#modal-d').text(text);
+            obj.html(obj.html().replace(/\n/g,'<br/>'));
+            $('#myModal').modal('show');
             console.log(data);
         }).fail(function (data) {
             $("form").html(
